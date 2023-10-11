e4bec73b8e01ee964a46e8d1672ffbfe505b4c3b
Leonardo <you@example.com>
2023-10-11 20:02:34 -0300

    adicionando projeto

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const content = document.querySelector(".content");
+const textFinish = document.querySelector(".textFinish");
+const contentFinish = document.querySelector(".finish");
+const spanQtd = document.querySelector(".spanQtd");
+const buttonRestart = document.querySelector(".finish button");
+
+import questions from './questions.js'; //questions retorna array de obj onde cada índice é uma question
+
+let currentQuestion = 0;
+let questionsCorrect = 0;
+
+const loadQuestion = () => {
+    spanQtd.innerHTML = `${currentQuestion + 1} / ${questions.length}`;
+    const item = questions[currentQuestion];
+    answers.innerHTML = "";
+    question.innerHTML = item.question;
+
+    item.answers.forEach(answer => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        <button class="answer" data-correct="${answer.correct}">
+        ${answer.option}
+        </button>`;
+
+        answers.appendChild(div);
+    });
+
+    document.querySelectorAll(".answer").forEach(item => {
+        item.addEventListener("click", nextQuestion);
+    })
+}
+
+const nextQuestion = event => {
+    const correctOrIncorrect = document.querySelector(".correctOrIncorrect");
+    if (event.target.getAttribute("data-correct") === "true") {
+        questionsCorrect++;
+        correctOrIncorrect.style.background = "green";
+    } 
+    else {
+        correctOrIncorrect.style.background = "red";
+    }
+
+    if (currentQuestion < questions.length - 1) { // verificando se é a ultima questão
+        currentQuestion++;
+        setTimeout(() => {
+            loadQuestion();
+            correctOrIncorrect.style.background = "gray";
+        }, 2000);
+    }
+    else {
+        setTimeout(() => finish(), 1500);
+    }
+}
+
+const finish = () => {
+    textFinish.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length}`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+}
+
+buttonRestart.onclick = () => {
+    content.style.display = "flex";
+    contentFinish.style.display = "none";
+
+    currentQuestion = 0;
+    questionsCorrect = 0;
+    loadQuestion();
+}
+
+console.log(questions);
+
+loadQuestion()
---
+++ src/questions.js
@@ -0,0 +1,75 @@
+export default [
+    {
+        question: "Qual a capital do Brasil?",
+        answers: [
+            { option: "Brasilia", correct: true },
+            { option: "São Paulo", correct: false },
+            { option: "Rio de Janeiro", correct: false}
+        ]
+    },
+    {
+        question: "Qual a capital do Rio de Janeiro?",
+        answers: [
+            { option: "Nova Iguaçu", correct: false },
+            { option: "Duque de Caxias", correct: false },
+            { option: "Rio de Janeiro", correct: true }
+        ]
+    },
+    {
+        question: "Qual dos países faz fronteira com o Brasil?",
+        answers: [
+            { option: "Nicarágua", correct: false },
+            { option: "Bolivia", correct: true },
+            { option: "Equador", correct: false}
+        ]
+    },
+    {
+        question: "Em que ano o Brasil se tornou independente?",
+        answers: [
+            { option: "1822", correct: true },
+            { option: "1501", correct: false },
+            { option: "1964", correct: false}
+        ]
+    },
+    {
+        question: "Qual a capital da Paraíba?",
+        answers: [
+            { option: "Filipéia", correct: false },
+            { option: "João Pessoa", correct: true },
+            { option: "Recife", correct: false }
+        ]
+    },
+    {
+        question: "Qual o Maior estado brasileiro em território?",
+        answers: [
+            { option: "Amazonas", correct: true },
+            { option: "São Paulo", correct: false },
+            { option: "Pará", correct: false }
+        ]
+    },
+    {
+        question: "Qual o estado mais novo do Brasil?",
+        answers: [
+            { option: "Sergipe", correct: false },
+            { option: "Roraima", correct: false },
+            { option: "Acre", correct: true }
+        ]
+    }, 
+    {
+        question: "Quantas línguas oficiais existem no Brasil?",
+        answers: [
+            { option: "2", correct: false },
+            { option: "1" , correct: true },
+            { option: "3" , correct: false }
+        ]
+    },
+    {
+        question: "Destas regiões brasileiras, qual a mais populosa?",
+        answers: [
+            { option: "Sul", correct: false },
+            { option: "Nordeste" , correct: true },
+            { option: "Norte" , correct: false }
+        ]
+        
+    }
+];
